ceb5711e82912ebef50358ad39e7e7fc0cd59479
Christofer Flores <cristofer.flort@gmail.com>
2019-10-07 15:07:26 -0500

    Add missing test to complete the bot controller coverage

--- tests/bot.test.js
+++ tests/bot.test.js
@@ -42,7 +42,6 @@ describe('Bot', () => {
     describe('Slack bot responses', () => {
       it('should respond with a 200 status only when subtype parameter is on the request', (done) => {
         const slackMessageApiMock = nock('https://slack.com/api/chat.postMessage')
-          .persist()
           .post('/')
           .query(true)
           .reply(200, { text: 'message published' })
@@ -60,7 +59,6 @@ describe('Bot', () => {
 
       it('should respond with a 200 status only when type parameter is different "message"', (done) => {
         const slackMessageApiMock = nock('https://slack.com/api/chat.postMessage')
-          .persist()
           .post('/')
           .query(true)
           .reply(200, { text: 'message published' })
@@ -145,5 +143,111 @@ describe('Bot', () => {
           })
       })
     })
+
+    describe('Slack bot add a place from facebook url', () => {
+      it('Should add a place and send a message to slack', (done) => {
+        const slackMessageApiMock = nock('https://slack.com/api/')
+          .get('/chat.postMessage')
+          .reply(() => {
+            return [
+              201,
+              {
+                statusCode: 200
+              },
+            ]
+          })
+        const fbMock = nock('https://graph.facebook.com/')
+          .get(/\/v4.0\/+[0-9]*$/gm)
+          .query(true)
+          .reply(200, {
+            name: 'tacos ahumados',
+            description: 'taco bueno',
+            cover: {
+              source: 'imagenchida.png'
+            },
+            link: 'https:fburl.com/tacosahumados',
+            phone: '123123124',
+            single_line_address: 'San fernando'
+          })
+
+        const fbIDMock = nock('https://graph.facebook.com/v4.0/')
+          .get('/search')
+          .query(true)
+          .reply(200, { data: [
+            {
+              id: '222222222'
+            }
+          ]})
+
+        chai.request(server)
+          .post('/api/bot')
+          .send({ event: {
+              type: 'message',
+              text: 'add https://www.facebook.com/Lasglorias.delacocina',
+              channel: 'EG2234GW'
+            }
+          })
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            expect(fbIDMock).to.have.been.requested
+            expect(fbMock).to.have.been.requested
+            expect(slackMessageApiMock).to.have.been.requested
+            done()
+          })
+      })
+
+      it('should show the newly place on the db', (done) =>{
+        chai.request(server)
+          .get('/api/places')
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            expect(res.body).to.have.length(11)
+            done()
+          })
+      })
+    })
+
+    describe('Slack bot wrong option', () => {
+      it('should send a message with the valid options and not add any place', (done) => {
+        const slackMessageApiMock = nock('https://slack.com/api/')
+          .get('/chat.postMessage')
+          .reply(() => {
+            return [
+              201,
+              {
+                statusCode: 200
+              },
+            ]
+          })
+
+        const fbIDMock = nock('https://graph.facebook.com/v4.0/')
+          .get('/search')
+          .query(true)
+          .reply(200, { data: [
+            {
+              id: '222222222'
+            }
+          ]})
+
+        chai.request(server)
+          .post('/api/bot')
+          .send({ event: {
+              type: 'message',
+              text: 'agregar https://www.facebook.com/Lasglorias.delacocina',
+              channel: 'EG2234GW'
+            }
+          })
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            expect(slackMessageApiMock).to.have.been.requested
+            expect(fbIDMock).to.not.have.been.requested
+            done()
+          })
+      })
+    })
+
   })
 })
